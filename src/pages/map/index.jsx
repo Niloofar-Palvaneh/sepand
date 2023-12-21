9e8f35fa6a9b468dd45757514ad75a40f35e9e74
niloofar-palvanehcls <niloofarpalvaneh@gmail.com>
2023-12-21 23:23:05 +0330

    change scrollbar

--- src/pages/map/index.jsx
+++ src/pages/map/index.jsx
@@ -1,19 +1,48 @@
 import Bests from "@/components/ui/Bests";
 import Validity from "@/components/ui/Validity";
+import { CiSearch } from "react-icons/ci";
+import Image from "next/image";
 
 export default function Map() {
     return (
         <>
             <div className="flex items-center justify-center mt-8 ">
-                <div className="grid grid-cols-4 gap-6 w-[80%] sm:w-full md:w-full p-2  sm:grid-cols-2 md:grid-cols-3">
-                    <TestBox />
-                    <TestBox />
-                    <TestBox />
-                    <TestBox />
-                    <TestBox />
-                    <TestBox />
-                    <TestBox />
-                    <TestBox />
+                <div className="sm:w-full md:w-full p-2 w-[90%] flex flex-col items-center gap-24 ">
+                    <div className="grid grid-cols-4 gap-12  sm:grid-cols-2 md:grid-cols-3">
+                        <TestBox />
+                        <TestBox />
+                        <TestBox />
+                        <TestBox />
+                        <TestBox />
+                        <TestBox />
+                        <TestBox />
+                        <TestBox />
+                    </div>
+                    <div className=" px-4 flex gap-12 bg-Bsepa-200 rounded-xl">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d103583.05416115272!2d51.367116800000005!3d35.7761024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e03c6f0000001%3A0xf71ac9f4f900d01c!2sQasr%20Garden%20Museum!5e0!3m2!1sen!2s!4v1703184853887!5m2!1sen!2s"
+                            className="border-4 overflow-hidden border-white rounded -mt-6 -mb-6" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <div className="w-1/2">
+                            <div className="flex items-end justify-between">
+                                <div className="bg-white flex items-center gap-2 text-gray-600 p-1 rounded-xl shadow">
+                                    <CiSearch className="text-2xl" />
+                                    <input className="bg-transparent text-end outline-none" type="text" placeholder="جستجو مرکز" />
+                                </div>
+                                <h2 className="border-b border-b-4 border-Ysepa-100 w-max p-1 text-Bsepa-300 font-bold text-xl">
+                                    فیلتر زیست بوم
+                                </h2>
+                            </div>
+                            <div className="mt-8 h-[320px] overflow-auto">
+                                <FilterTable/>
+                                <FilterTable/>
+                                <FilterTable/>
+                                <FilterTable/>
+                                <FilterTable/>
+                                <FilterTable/>
+                                <FilterTable/>
+                                <FilterTable/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Bests />
@@ -21,6 +50,7 @@ export default function Map() {
         </>
     )
 }
+
 export function TestBox() {
     return (
         <>
@@ -30,5 +60,20 @@ export function TestBox() {
                 </h5>
             </div>
         </>
+    )
+}
+export function FilterTable() {
+    return (
+        <div className="w-full flex items-center p-1 justify-between border-b border-gray-500">
+            <div className="flex flex-col items-center">
+                <span>120</span>
+                <span className="text-[15px] text-gray-600">رتبه</span>
+            </div>
+            <h3>شتاب دهنده سپند</h3>
+            <div className="flex flex-col items-center">
+                <Image className="bg-white p-1 rounded" src={"/imgs/m-1.png"} width={30} height={0} alt="..." />
+                <h2 className="text-[12px] text-gray-500">مرکز اصفهان</h2>
+            </div>
+        </div>
     )
 }
