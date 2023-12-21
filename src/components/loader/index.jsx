0d67e083449feac550b46b0db3154b11863457d3
niloofar-palvanehcls <niloofarpalvaneh@gmail.com>
2023-12-21 17:55:23 +0330

    create loader

---
+++ src/components/loader/index.jsx
@@ -0,0 +1,12 @@
+
+export default function Loader (){
+    return(
+        <div id="loading-container">
+        <div id="loading-dots">
+          <div class="loading-dot"></div>
+          <div class="loading-dot"></div>
+          <div class="loading-dot"></div>
+        </div>
+      </div>
+    )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,33 @@
 import Footer from '@/components/globals/Footer'
 import Navbar from '@/components/globals/Navbar'
 import '@/styles/globals.css'
-
+import Router from "next/router";
+import { useState } from 'react';
+import Loader from '@/components/loader';
 export default function App({ Component, pageProps }) {
+  const [loading, setloading] = useState(false);
+  Router.events.on("routeChangeStart", (url) => {
+    setloading(true);
+  });
+  Router.events.on("routeChangeError", (url) => {
+    setloading(false);
+  });
+  Router.events.on("routeChangeComplete", (url) => {
+    setloading(false);
+  });
   return (
     <>
-    <Navbar/>
-    <Component {...pageProps} />
-    <Footer/>
+      {
+        loading == false ? (
+          <>
+            <Navbar />
+            <Component {...pageProps} />
+            <Footer />
+          </>
+        ) : (
+          <Loader />
+        )
+      }
     </>
   )
 }
