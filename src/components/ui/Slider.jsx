45863cd9c032e5bb8090f3a76f162a79ce342ce9
niloofar-palvanehcls <niloofarpalvaneh@gmail.com>
2023-12-22 15:11:45 +0330

    autoplay slider

--- src/components/ui/Slider.jsx
+++ src/components/ui/Slider.jsx
@@ -3,10 +3,41 @@ import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import Button from "./Button"
 
-export default function Slider({link}) {
+export default function Slider({ link }) {
     const [sliderRef] = useKeenSlider({
         loop: true,
-    })
+    },
+        [
+            (slider) => {
+                let timeout
+                let mouseOver = false
+                function clearNextTimeout() {
+                    clearTimeout(timeout)
+                }
+                function nextTimeout() {
+                    clearTimeout(timeout)
+                    if (mouseOver) return
+                    timeout = setTimeout(() => {
+                        slider.next()
+                    }, 3000)
+                }
+                slider.on("created", () => {
+                    slider.container.addEventListener("mouseover", () => {
+                        mouseOver = true
+                        clearNextTimeout()
+                    })
+                    slider.container.addEventListener("mouseout", () => {
+                        mouseOver = false
+                        nextTimeout()
+                    })
+                    nextTimeout()
+                })
+                slider.on("dragStarted", clearNextTimeout)
+                slider.on("animationEnded", nextTimeout)
+                slider.on("updated", nextTimeout)
+            },
+        ]
+    )
 
     return (
         <div className="flex items-center justify-center">
