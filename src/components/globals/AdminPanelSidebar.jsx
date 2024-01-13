38dde7b08d269cd4bc730211f997977d759d6ab9
niloofar-palvanehcls <niloofarpalvaneh@gmail.com>
2024-01-13 23:17:13 +0330

    create pagination for users section

--- src/components/globals/AdminPanelSidebar.jsx
+++ src/components/globals/AdminPanelSidebar.jsx
@@ -4,28 +4,35 @@ import { FaUsers } from "react-icons/fa";
 import { FaUserSecret } from "react-icons/fa";
 import { MdArticle } from "react-icons/md";
 import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function AdminPanelSidebar() {
+    const router = useRouter()
     const menus = [
         {
             id: 1,
             title: "داشبورد",
-            icon: <MdDashboard />
+            icon: <MdDashboard />,
+            href: "/adminpanel"
         },
         {
             id: 2,
             title: "کاربران",
-            icon: <FaUsers />
+            icon: <FaUsers />,
+            href: "/adminpanel/users"
         },
         {
             id: 4,
             title: "ادمین ها",
-            icon: <FaUserSecret />
+            icon: <FaUserSecret />,
+            href: "/adminpanel/users"
         },
         {
             id: 5,
             title: "مقالات",
             icon: <MdArticle />,
+            href: "/adminpanel/users"
         }
     ]
     const [activeMenu, setActiveMenu] = useState(1)
@@ -43,13 +50,13 @@ export default function AdminPanelSidebar() {
                     <ul className="flex justify-end items-end gap-4 flex-col" >
                         {
                             menus.map(item => (
-                                <li
+                                <Link href={item.href}
                                     onClick={() => setActiveMenu(item.id)}
                                     key={item.id}
                                     className={`${item.id === activeMenu && "bg-gray-100"} flex items-center justify-end gap-2 text-gray-700 cursor-pointer transition hover:bg-gray-100 w-full rounded p-2`}>
                                     {item.title}
                                     {item.icon}
-                                </li>
+                                </Link>
                             ))
                         }
                     </ul>
---
+++ src/pages/adminpanel/[users].jsx
@@ -0,0 +1,115 @@
+import AdminPanelSidebar from "@/components/globals/AdminPanelSidebar";
+import { useEffect, useState } from "react";
+
+
+export default function users() {
+    const users = [
+        {
+            id: 1,
+            name: "نیلوفر",
+            city: "کرمانشاه"
+        },
+        {
+            id: 2,
+            name: "زهرا",
+            city: "تهران"
+        },
+        {
+            id: 3,
+            name: "سارا",
+            city: "کرمان"
+        },
+        {
+            id: 4,
+            name: "لیلا",
+            city: "اهواز"
+        },
+        {
+            id: 5,
+            name: "فرشته",
+            city: "تهران"
+        },
+        {
+            id: 6,
+            name: "دنیا",
+            city: "مشهد"
+        },
+        {
+            id: 7,
+            name: "درسا",
+            city: "قزوین"
+        },
+        {
+            id: 8,
+            name: "سمیرا",
+            city: "رشت"
+        },
+        {
+            id: 9,
+            name: "غزل",
+            city: "مشهد"
+        },
+        {
+            id: 10,
+            name: "درسا",
+            city: "قزوین"
+        },
+    ]
+    const [paginatedUsers, setPaginatedUsets] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+    let pageSize = 4
+    let pagesNumber
+    const pageCount = Math.ceil(users.length / pageSize)
+    pagesNumber = Array.from(Array(pageCount).keys())
+    useEffect(() => {
+        let endIndex = pageSize * currentPage
+        let startIndex = endIndex - pageSize
+        let shownUsers = users.slice(startIndex, endIndex)
+        setPaginatedUsets(shownUsers)
+    }, [currentPage])
+
+    const changePageHandler = (newPage) => {
+        setCurrentPage(newPage)
+        let endIndex = pageSize * currentPage
+        let startIndex = endIndex - pageSize
+        let shownUsers = users.slice(startIndex, endIndex)
+        setPaginatedUsets(shownUsers)
+    }
+    return (
+        <>
+            <div className="flex items-center justify-center mt-8">
+                <div className="w-[80%] flex gap-4 sm:flex-col-reverse sm:w-[98%] md:w-[97%] xl:w-[98%]">
+                    <div className="w-full flex flex-col gap-2">
+                        <div className="flex items-center justify-around bg-blue-100 p-2 rounded font-bold">
+                            <span className="text-end w-[60px]">شهر</span>
+                            <span className="text-end w-[60px]">نام</span>
+                            <span className="text-end w-[60px]">ردیف</span>
+                        </div>
+                        {
+                            paginatedUsers.map(user => (
+                                <div className="flex items-center justify-around bg-white p-1 rounded 
+                                shadow border text-gray-500 hover:text-gray-900 hover:bg-Bsepa-100">
+                                    <span className="text-end w-[60px]">{user.city}</span>
+                                    <span className="text-end w-[60px]">{user.name}</span>
+                                    <span className="text-end w-[60px]">{user.id}</span>
+                                </div>
+                            ))
+                        }
+                        <div className="flex flex-row-reverse items-center justify-center gap-4">
+                            {
+                                pagesNumber.map(item => (
+                                    <button
+                                        className={`
+                                        px-4 py-2 rounded shadow ${currentPage === item + 1 && "bg-Bsepa-300 text-white"}`}
+                                        onClick={() => changePageHandler(item + 1)}
+                                    >{item + 1}</button>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <AdminPanelSidebar />
+                </div>
+            </div>
+        </>
+    )
+}
